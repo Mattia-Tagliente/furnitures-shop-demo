29fc2acf116267f363fc6d3043c4b94cc5d41d37
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-10-22 14:50:13 +0200

    feat: creates the index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Furniking</title>
+</head>
+<body>
+    <header class="furniking-header">
+        <div class="login-languages-bar">
+            <p>Welcome to our online shop</p>
+            <div class=languages-login>
+                <div class="languages-choice">
+                    <select name="languages" id="languages">
+                        <option value="english">English</option> 
+                        <option value="german">Deutsch</option> 
+                        <option value="french">Française</option> 
+                        <option value="italian">Italiano</option> 
+                        <option value="spanish">Español</option> 
+                    </select>
+                </div>
+            </div>
+        </div>
+        <header>
+</body>
+</html>
