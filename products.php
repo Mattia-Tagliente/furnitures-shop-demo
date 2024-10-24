b51ff587af341ca622550cc9ee66e1b25feb684e
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-10-24 15:51:26 +0200

    feat: refactors products.php to show only n furnitures per page

--- products.php
+++ products.php
@@ -9,78 +9,115 @@
     ?>
 </head>
 <body>
-
-    <table>
     <?php
-        $selectFurnitureQuery = "SELECT furniture.furniture_name, furniture_type.type_name, 
-        furniture_picture.picture_path, furniture_price.price
+        
+        $maxProductsPerPage = 4;
+        $productPages = [];
+
+        $NumberFurnitureQuery = "SELECT COUNT(*) AS number_products
         FROM furniture_picture 
         INNER JOIN furniture ON furniture_picture.furniture_id = furniture.furniture_id
         INNER JOIN furniture_type ON furniture_type.type_id = furniture.furniture_type
         INNER JOIN furniture_price ON  furniture_price.furniture_id = furniture.furniture_id
         WHERE furniture_price.end_date IS null
-        LIMIT 3"
+        "
         ;
-    
 
-        // Per mostrare solo due o n prodotti per pagina:
-
-        // innanzitutto vediamo quante pagine avremo in base al numero totale dei prodotti restituiti dalla query
-        // crieamo un array contenente il numero delle pagine che ha solo all'inizio il numero 1
-        // prendere il numero totale degli items
-        // se il numero totale dei prodotti è maggiore al limite per pagina: 
-        // ciclo while (condizione: numero delle pagine > 0)
-        // in cui ad ogni iterazione totale prodotti - numero prodotti per pagina,
-        //  numero della pagina += 1
-        //  numero prodotti -= numero prodotti per pagina
-
-        // se la dimensione dell'array delle pagine è maggiore:
-        // stampare i numeri delle pagine sotto 
-        // se la variabile pagina non è null --- > (ad esempio è 1, vedere sotto)
-        // limit della query da (limite per pagina * numero della pagina + 1), prendere 2
-        // ciclo for (per il numero di pagine nell'array)
-        // stampa un link con numero della pagina, sia come testo che nella variabile pagina dell'href ad ogni iterazione
-
-        // altrimenti, se la dimensione dell'array è solo 1 di uno, semplicemente stampare i prodotti
-        // senza particolari seghe mentali
-
-        $SelectPicturesResult = $conn->query($selectFurnitureQuery);
-
-        while ($fetchRow = $SelectPicturesResult->fetch_array()){
-
-            $picturePath = $fetchRow['picture_path'];
-            $furnitureName =$fetchRow['furniture_name'];
-            $furnitureType =$fetchRow['type_name'];
-            $furniturePrice =$fetchRow['price'];
-
-            $ratingQuery = "SELECT AVG(vote) AS rating_vote
-            FROM furniture_rating 
-            INNER JOIN furniture ON furniture_rating.furniture = furniture.furniture_id
-            WHERE furniture.furniture_name = '$furnitureName'";
-
-            $ratingResult = $conn->query($ratingQuery);
-            $fetchRating =  $ratingResult->fetch_array();
-
-            $furnitureRating = ceil($fetchRating['rating_vote']);
-
-            echo "
-            <td>
-            <img src=$picturePath width='200px' height='200px'> 
-            <p>Type:$furnitureType </p>
-            <p>Name:$furnitureName </p>
-            <p>Price:$furniturePrice</p>
-            ";
-
-            $ratingString = "";
-
-            for ($i = 0; $i < $furnitureRating; $i++){
-                $ratingString = $ratingString."*";
-            }
+        $numberProductsResult = $conn->query($NumberFurnitureQuery);
+        $fetchNumberProducts =  $numberProductsResult ->fetch_array();
+        $numberProducts = $fetchNumberProducts['number_products'];
             
-            echo "<p>Rating:$ratingString</p>
-            <td>";
+
+            //number of the page
+            $pageNumber = $_GET["p"];
+            $offset = ($pageNumber - 1) * $maxProductsPerPage;
+
+            $selectFurnitureQuery = "SELECT furniture.furniture_name, furniture_type.type_name, 
+                furniture_picture.picture_path, furniture_price.price
+                FROM furniture_picture 
+                INNER JOIN furniture ON furniture_picture.furniture_id = furniture.furniture_id
+                INNER JOIN furniture_type ON furniture_type.type_id = furniture.furniture_type
+                INNER JOIN furniture_price ON  furniture_price.furniture_id = furniture.furniture_id
+                WHERE furniture_price.end_date IS null
+                LIMIT $offset, $maxProductsPerPage";
+
+            if ($pageNumber == null){
+            $selectFurnitureQuery = "SELECT furniture.furniture_name, furniture_type.type_name, 
+                furniture_picture.picture_path, furniture_price.price
+                FROM furniture_picture 
+                INNER JOIN furniture ON furniture_picture.furniture_id = furniture.furniture_id
+                INNER JOIN furniture_type ON furniture_type.type_id = furniture.furniture_type
+                INNER JOIN furniture_price ON  furniture_price.furniture_id = furniture.furniture_id
+                WHERE furniture_price.end_date IS null
+                LIMIT $maxProductsPerPage";
+            }                
+
+$SelectPicturesResult = $conn->query($selectFurnitureQuery);
+
+echo "<table>";
+while ($fetchRow = $SelectPicturesResult->fetch_array()){
+
+    $picturePath = $fetchRow['picture_path'];
+    $furnitureName =$fetchRow['furniture_name'];
+    $furnitureType =$fetchRow['type_name'];
+    $furniturePrice =$fetchRow['price'];
+
+    $ratingQuery = "SELECT AVG(vote) AS rating_vote
+    FROM furniture_rating 
+    INNER JOIN furniture ON furniture_rating.furniture = furniture.furniture_id
+    WHERE furniture.furniture_name = '$furnitureName'";
+
+    $ratingResult = $conn->query($ratingQuery);
+    $fetchRating =  $ratingResult->fetch_array();
+
+    $furnitureRating = ceil($fetchRating['rating_vote']);
+
+    echo "
+    <td>
+    <img src=$picturePath width='200px' height='200px'> 
+    <p>Type:$furnitureType </p>
+    <p>Name:$furnitureName </p>
+    <p>Price:$furniturePrice</p>
+    ";
+
+    $ratingString = "";
+
+    for ($i = 0; $i < $furnitureRating; $i++){
+        $ratingString = $ratingString."*";
+    }
+    
+    echo "<p>Rating:$ratingString</p>
+    <td>"
+    ;
+}
+echo "</table><br>";
+
+//This cycle populates the array with the number of pages based on the number of products
+if ($numberProducts > $maxProductsPerPage){
+    while ($numberProducts > 0){
+        $numberProducts -= $maxProductsPerPage;
+        $nextPage = end($productPages) + 1;
+        $productPages[] = $nextPage;
+    }
+
+    for ($i = 0; $i < count($productPages); $i++){
+        $pageIndex = $productPages[$i];
+        if ($pageIndex == 1){
+            echo "
+            <a href='./products.php'>$pageIndex</a>
+            " 
+            ;
+        } else {
+            echo "
+            <a href='./products.php?p=$pageIndex'>$pageIndex</a>
+            " 
+            ;
         }
-    ?>
-    </table>
+        
+        
+    }
+}
+        
+?>
 </body>
 </html>
